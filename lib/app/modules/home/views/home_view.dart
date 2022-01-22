e128227096a218bcecd20a3968f301a36981ee82
Your Name <you@example.com>
2022-01-23 00:09:44 +0600

    Contributed by Yeaseen AHmed

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -12,11 +12,126 @@ class HomeView extends GetView<HomeController> {
         title: Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "বাংলা স্বরবর্ণ",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(50),
+                        side: BorderSide(
+                          width: 5,
+                          color: Colors.yellowAccent,
+                        ),
+                        primary: Colors.green,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 45,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "বাংলা  ব্যঞ্জনবর্ণ",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(50),
+                        side: BorderSide(
+                          width: 5,
+                          color: Colors.yellowAccent,
+                        ),
+                        primary: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 45,
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "ENGLISH ALPHABETS",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(50),
+                        side: BorderSide(
+                          width: 5,
+                          color: Colors.yellowAccent,
+                        ),
+                        primary: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 45,
+                ),
+                Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "(1-100) NUMBERS",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(50),
+                        side: BorderSide(
+                          width: 5,
+                          color: Colors.yellowAccent,
+                        ),
+                        primary: Colors.green,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "(১-১০০) সংখ্যা",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.all(50),
+                        side: BorderSide(
+                          width: 5,
+                          color: Colors.yellowAccent,
+                        ),
+                        primary: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
